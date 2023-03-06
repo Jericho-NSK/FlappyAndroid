422cdd4b1eb6c9304f8a60bfd7cd9aba18c73390
Jericho-NSK <panchenkodv@ya.ru>
2023-03-06 19:02:33 +0700

    update font

--- elements.py
+++ elements.py
@@ -1,20 +1,20 @@
-from constants import FONT, WIDTH, HEIGHT
-from images_and_sounds import Images
+from constants import WIDTH, HEIGHT
+from design import Images, Text
 from walls import Wall
 
 
 class Elements:
 
-    def __init__(self, game, window):
-        self.score = FONT.render(f'SCORE: {game.score}', True, 'red')
-        self.speed = FONT.render(f'SPEED: {Wall.speed}', True, 'red')
+    def __init__(self, game):
+        self.score = Text.font_window.render(f'SCORE: {game.score}', True, 'red')
+        self.speed = Text.font_window.render(f'SPEED: {Wall.speed}', True, 'red')
 
-        self.escape_up = FONT.render('NO PAUSES! ONLY HARDCORE!', True, 'red')
-        self.escape_down = FONT.render('PRESS ESCAPE AGAIN TO GIVE UP', True, 'red')
+        self.escape_up = Text.font_window.render('NO PAUSES! ONLY HARDCORE!', True, 'red')
+        self.escape_down = Text.font_window.render('PRESS BACK AGAIN TO GIVE UP', True, 'red')
         self.escape_up_rect = self.escape_up.get_rect()
-        self.escape_up_rect.center = WIDTH //2, HEIGHT - 3 * self.escape_up_rect.h
+        self.escape_up_rect.center = WIDTH // 2, HEIGHT - 3 * self.escape_up_rect.h
         self.escape_down_rect = self.escape_down.get_rect()
-        self.escape_down_rect.center = WIDTH //2, HEIGHT - 2 * self.escape_down_rect.h
+        self.escape_down_rect.center = WIDTH // 2, HEIGHT - 2 * self.escape_down_rect.h
         self.escape_timer = 0
 
         self.heart_rect = Images.heart.get_rect()
