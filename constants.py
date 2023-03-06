730cc82f608727be3bd76857da191655a3a5589a
Jericho-NSK <panchenkodv@ya.ru>
2023-03-06 19:03:16 +0700

    update window for android

--- constants.py
+++ constants.py
@@ -4,25 +4,35 @@ import pygame
 
 pygame.init()
 
-print(pygame.font.get_fonts())
-print(pygame.font.get_default_font())
+SPEED = 10
+
 if system() != 'Windows':
-    WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
+    HEIGHT = 720
+    WIDTH = HEIGHT * pygame.display.Info().current_w / pygame.display.Info().current_h * 0.97
+    WINDOW = pygame.display.set_mode(size=(WIDTH, HEIGHT),
+                                     flags=pygame.FULLSCREEN | pygame.SCALED,
+                                     depth=32,
+                                     vsync=True,
+                                     )
+
 else:
-    WIDTH, HEIGHT = 1280, 720
+    HEIGHT = 720
+    WIDTH = HEIGHT * pygame.display.Info().current_w / pygame.display.Info().current_h
+    WINDOW = pygame.display.set_mode(size=(WIDTH, HEIGHT),
+                                     flags=pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.NOFRAME,
+                                     depth=32,
+                                     vsync=True,
+                                     )
+
 
 START_TRACK = HEIGHT // 2.5
 BIRD_SIZE = 40, 30
 BIRD_START = WIDTH // 10, HEIGHT // 3
 START_LIVES = 3
 FPS = 60
-SPEED = 2
-DIFFICULTY_MODS = (1, 10, 20, 35, 55, 75, 100, 130, 165, 200)
-FONT = pygame.font.SysFont('comicsanms', size=48, italic=True)
 
-WINDOW = pygame.display.set_mode(size=(WIDTH, HEIGHT),
-                                 flags=pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.NOFRAME,
-                                 depth=32,
-                                 vsync=True,
-                                 )
+DIFFICULTY_MODS = (1, 10, 20, 35, 55, 75, 100, 130, 165, 200)
+
+GITHUB_LINK = 'https://github.com/Jericho-NSK/FlappyAndroid'
+
 CLOCK = pygame.time.Clock()
