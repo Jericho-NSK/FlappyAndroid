1ed8d018cc29c521452f7658a1487b7282006e24
Jericho-NSK <panchenkodv@ya.ru>
2023-03-10 00:50:59 +0700

    added jump buttons

--- window.py
+++ window.py
@@ -1,6 +1,6 @@
 import pygame
 
-from constants import WIDTH, FPS, WINDOW, CLOCK, HEIGHT
+from constants import WIDTH, FPS, WINDOW, CLOCK
 from design import Images
 from elements import Elements
 
@@ -10,7 +10,7 @@ class Window:
 
     def __init__(self, game):
         self.bg_rect = Images.bg.get_rect()
-        self.elements = Elements(game, self)
+        self.elements = Elements(game)
 
     def update_window(self, game):
         self.window.blit(Images.bg, (self.bg_rect.x, 0))
@@ -24,35 +24,34 @@ class Window:
             (
                 (self.elements.score, (20, 20)),
                 (self.elements.speed, (20, 60)),
-                *(
-                    (Images.heart,
-                     (
-                         WIDTH - 1.15 * self.elements.heart_rect.w * (heart + 1),
-                         self.elements.heart_rect.y)) for heart in range(game.lives)
-                ),
+                *((Images.heart,
+                   (WIDTH - 1.15 * self.elements.heart_rect.w * (heart + 1),
+                    self.elements.heart_rect.y)) for heart in range(game.lives)),
                 (game.bird.image, (game.bird.rect.centerx, game.bird.rect.centery)),
             ),
-            False
-        )
+            doreturn=False)
 
         if self.elements.escape_timer:
             self.window.blit(self.elements.escape_up, self.elements.escape_up_rect.topleft)
             self.window.blit(self.elements.escape_down, self.elements.escape_down_rect.topleft)
             self.elements.escape_timer -= 1
+
         self.control_buttons()
         if game.bird.image != Images.bird_images[-1]:
             game.bird.flying(game)
         if game.game_starts:
-
             pygame.display.update()
-
-
 
         CLOCK.tick(FPS)
 
     def control_buttons(self):
-        # self.window.blit(Images.big_jump, self.elements.big_jump_surf)
-        # self.window.blit(Images.small_jump, self.elements.small_jump_surf)
-        self.window.blit(self.elements.surf, self.elements.surfr)
-        self.window.blit(Images.big_jump, (self.elements.surfr.centerx - Images.big_jump.get_width()//2,
-                                           self.elements.surfr.centery - Images.big_jump.get_height()//2))
+        self.window.blits(
+            (
+                (self.elements.big_jump_bg, self.elements.big_jump_bg_rect),
+                (Images.big_jump, self.elements.big_jump.topleft),
+                (self.elements.left_jump_bg, self.elements.left_jump_bg_rect),
+                (Images.small_jump, self.elements.left_jump.topleft),
+                (self.elements.right_jump_bg, self.elements.right_jump_bg_rect),
+                (Images.small_jump, self.elements.right_jump.topleft),
+            ),
+            doreturn=False)
